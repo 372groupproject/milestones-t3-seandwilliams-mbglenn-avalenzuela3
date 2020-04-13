8eed6545bd949d8dd5ecad725cbd950de3f22482
SeanDWill <51764867+SeanDWill@users.noreply.github.com>
2020-04-12 21:23:13 -0700

    Create floats.cbl

---
+++ code/floats.cbl
@@ -0,0 +1,18 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. FLOAT-TYPE.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+01 ValueX PIC 9(3)V99.
+
+PROCEDURE DIVISION.
+DISPLAY "We will show how basic primitive floats work in COBOL ValueX = "ValueX.
+ADD 35 TO ValueX GIVING ValueX.
+DISPLAY "After adding 35 to ValueX, ValueX = "ValueX.
+MULTIPLY Valuex BY 10 GIVING ValueX.
+DISPLAY "After multiplying by ten to ValueX, ValueX = "ValueX.
+ADD .15 TO ValueX GIVING ValueX.
+DISPLAY "After adding .15 to ValueX, ValueX = "ValueX.
+MULTIPLY ValueX BY 2 GIVING ValueX.
+DISPLAY "After multiplying by two to ValueX, ValueX = "ValueX.
+STOP RUN.
