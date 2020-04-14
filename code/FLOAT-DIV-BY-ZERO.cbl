f47f0dd725f786d5da9964cc9e993e261b231eeb
SeanDWill <51764867+SeanDWill@users.noreply.github.com>
2020-04-13 22:04:06 -0700

    Update and rename FLOAT-TYPE-ERR.cbl to FLOAT-DIV-BY-ZERO.cbl

--- code/FLOAT-DIV-BY-ZERO.cbl
+++ code/FLOAT-DIV-BY-ZERO.cbl
@@ -1,5 +1,5 @@
 IDENTIFICATION DIVISION.
-PROGRAM-ID. FLOAT-TYPE-ERR.
+PROGRAM-ID. FLOAT-DIV-BY-ZERO.
 DATA DIVISION.
 WORKING-STORAGE SECTION.
 01 ValueX PIC 9(3)V99.
@@ -13,6 +13,10 @@ ADD .15 TO ValueX GIVING ValueX.
 DISPLAY "After adding .15 to ValueX, ValueX = "ValueX.
 MULTIPLY ValueX BY 2 GIVING ValueX.
 DISPLAY "After multiplying by two to ValueX, ValueX = "ValueX.
-SET "HELLO WORLD!" TO ValueX.
-DISPLAY "After setting 'HELLO WORLD!' to ValueX, ValueX = "ValueX.
+MULTIPLY ValueX BY 0 GIVING ValueX.
+DISPLAY "After multiplying by 0 to ValueX, ValueX = "ValueX.
+ADD 1000000000001 TO ValueX.
+DISPLAY "After adding 1 trillion to ValueX, ValueX = "ValueX.
+DIVIDE 99 BY 0 GIVING ValueX.
+DISPLAY "After dividing 99 by 0 giving ValueX, ValueX = "ValueX.
 STOP RUN.
